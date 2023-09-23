cf521e03e857d29dc648f6d3dba031dafdc956d1
PSuade <141860763+PSuade@users.noreply.github.com>
2023-09-23 10:29:22 -0400

    Updated CPL_scanner.py

--- CPL_scanner.py
+++ CPL_scanner.py
@@ -1,14 +1,55 @@
-from token import *
+from Token import *
 import json
 import sys
+import re
 
 # Group members (Patrick Mahon, Conner Lord, Jonathan Lin)
 
 def remove_items(test_list, item):
-    #using list comprehension to perform the task
-    res - [i for i in test_list if i != item]
-
+    # Using list comprehension to perform the task
+    res = [i for i in test_list if i != item]
     return res
+
+def tokenize_line(line):
+    tokens = []
+    current_token = ""
+    in_string = False
+
+    # Define regular expressions for different token types
+    keyword_regex = r'\b(import|implementations|function|main|is|variables|define|of|begin|display|set|input|if|then|else|endif|not|greater|or|equal|return)\b'
+    string_literal_regex = r'("[^"]*"|\'[^\']*\')'
+    identifier_regex = r'[a-zA-Z_]\w*'
+    numeric_literal_regex = r'\b\d+(\.\d+)?\b'
+    
+    # Combine all the regular expressions
+    regex = f'({keyword_regex})|({string_literal_regex})|({identifier_regex})|({numeric_literal_regex})|(\S)'
+
+    matches = re.finditer(regex, line)
+
+    for match in matches:
+        token = match.group(0)
+        
+        if token.strip() == "":
+            continue  # Ignore whitespace tokens
+        
+        if token.startswith('"') or token.startswith("'"):
+            if in_string:
+                in_string = False
+                current_token += token
+                tokens.append(current_token)
+                current_token = ""
+            else:
+                in_string = True
+                current_token = token
+        else:
+            if in_string:
+                current_token += token
+            else:
+                tokens.append(token)
+
+    return tokens
+
+
 
 def filter_file(File_name):
     try:
@@ -16,114 +57,20 @@ def filter_file(File_name):
     except:
         print("no such file or directory: ", File_name)
         exit(2)
-    # Filtering off
-    # Block comments
-    # Empty Lines
-    # Empty Spaces
-    # Line Comments
 
-    Comment = False
     lineList = []
 
     for line in file:
-        lineTokens = []
+        if line.startswith("/*"):
+            continue  # Skip the comment line
 
-        #create string statement tokens using "
-        if "" in line:
-            splitLocation = line.find("")
-            beforeStr = line[:splitLocation]
-            afterStr = line[splitLocation:]
-            secondSplitLocation = splitLocation + afterStr[1:].find("") + 1
-            strStatement = line[splitLocation:secondSplitLocation + 1]
-            afterStr = line[secondSplitLocation + 1:]
-
-            beforeStatementTokens = beforeStr.split(' ')
-            for token in beforeStatementTokens:
-                lineTokens.append(token)
-
-            lineTokens.append(strStatement)
-
-            if afterStr != '\n':
-                afterStatementTokens = afterStr.split(' ')
-                for token in afterStatementTokens:
-                    lineTokens.append(token)
-
+        lineTokens = tokenize_line(line)
+        
+        # Remove empty tokens or tokens containing only whitespace
+        lineTokens = [token for token in lineTokens if token.strip() != '']
+        
+        if lineTokens:
             lineList.append(lineTokens)
-            continue
-
-        #TODO: turn this into a callable function
-        # Split statement tokens using ^
-
-        if '^' in line:
-            splitLocation = line.find('^')
-            beforeStr = line[:splitLocation]
-            afterStr = line[splitLocation:]
-            secondSplitLocation = splitLocation + afterStr[1:].find('^') + 1
-            strStatement = line[splitLocation:secondSplitLocation + 1]
-            afterStr = line[secondSplitLocation + 1:]
-
-            beforeStatementTokens =  beforeStr.split(' ')
-            for token in beforeStatementTokens:
-                lineTokens.append(token)
-
-            lineTokens.append(strStatement)
-
-            afterStatementTokens = afterStr.split(' ')
-            for token in afterStatementTokens:
-                lineTokens.append(token)
-
-            lineList.append(lineTokens)
-            continue
-
-        #split lines into line tokens using ' '
-        lineTokens = line.split(' ')
-
-        #filters block comments (Proof of concept, need to implement temp for changes)
-        commentStart = "/*"
-        commentEnd = "*/"
-
-        if commentStart in line:
-            Comment = True
-
-        if not Comment:
-            lineList.append(lineTokens)
-
-        if commentEnd in line:
-            Comment = False
-        #End of block comment filter
-
-    #Empty space token filter
-    loopCount = 0
-    for line in lineList:
-        lineList[loopCount] = remove_items(line, '')
-        loopCount += 1
-
-    # \n filter
-    loopCount = 0
-    for line in lineList:
-        if '\n' in line[len(line) - 1]:
-            modifiedStr = line[len(line) - 1]
-            modifiedStr = modifiedStr[:-1]
-            line[len(line) - 1] = modifiedStr
-            lineList[loopCount] = line
-        loopCount += 1
-
-    # line comment filter
-    loopCount = 0
-    for line in lineList:
-        if '//' in line:
-            line = line[:line.index('//')]
-            lineList[loopCount] = line
-        loopCount += 1
-
-    currentLineNum = 0
-    while currentLineNum < len(lineList):
-        if lineList[currentLineNum[0]] == '':
-            lineList.remove(lineList[currentLineNum])
-        else:
-            currentLineNum += 1
-
-    lineList = list(filter(None, lineList))
 
     return lineList
 
@@ -141,41 +88,53 @@ def Convert(a):
     res_dct = dict(zip(it, it))
     return res_dct
 
-# adds two dictionary together
+# adds two dictionaries together
 def merge_dictionaries(dict1, dict2):
     merge_dict = dict1.copy()
     merge_dict.update(dict2)
-    return merged_dict
+    return merge_dict
 
+# Function to categorize tokens
+def categorize_token(token):
+    if token.isspace():
+        return {"Type": "Whitespace", "id": 999, "value": token}
+    elif token == "EOS":
+        return {"Type": "EndOfStatement", "id": 1000, "value": token}
+    elif token in ["/*", "*/"]:
+        return {"Type": "Comment", "id": 1001, "value": token}
+    elif token in ["import", "implementations", "function", "main", "is", "variables", "define", "of", "begin", "display", "set", "input", "if", "then", "else", "endif", "not", "greater", "or", "equal", "return"]:
+        return {"Type": "Keyword", "id": 2000, "value": token}
+    elif re.match(r'^[a-zA-Z_]\w*$', token):
+        return {"Type": "Identifier", "id": 3000, "value": token}
+    elif re.match(r'^[0-9]+(\.[0-9]+)?$', token):
+        return {"Type": "NumericLiteral", "id": 4000, "value": token}
+    elif token.startswith('"') or token.startswith("'"):
+        return {"Type": "StringLiteral", "id": 5000, "value": token}
+    elif token in [":", ".", ":", ",", "/", "=", ">", "*", ")"]:
+        if token in [":", ",", "=", ")"]:
+            return {"Type": "Operator", "id": 6000, "value": token}
+        elif token in [".", "/", "*", ">"]:
+            return {"Type": "Operator", "id": 6001, "value": token}
+        elif token == ":":
+            return {"Type": "VariableDeclaration", "id": 6002, "value": token}
+    # Add more conditions for other types of tokens
 
-# main scanner method 
+    # If none of the above conditions match, treat it as an "UNKNOWN" token
+    return {"Type": "UNKNOWN", "id": 1200, "value": token}
+
 if __name__ == '__main__':
-    sysArgv = sys.argv # scan file from sys argvs
+    sysArgv = sys.argv
 
-    ItemList = filter_file(sysArgv[1]) # create token list from file
+    ItemList = filter_file(sysArgv[1])
 
     finalTokenList = []
+
     megaDict = {}
 
-    # turn all tokens in list to appropriate token objs
-    # print said tokens to console
-    for Items in ItemList:
-        for TokenItem in Items:
-            if TokenItem in tokenList["keywords"]:
-                newToken = Token("keywords", tokenList["keywords"][TokenItem], TokenItem)
-            elif TokenItem in tokenList["identifiers"]:
-                newToken = Token("identifiers", tokenList["identifiers"][TokenItem], TokenItem)
-            elif TokenItem in tokenList["operators"]:
-                newToken = Token("operators", tokenList["operators"][TokenItem], TokenItem)
-            elif TokenItem in tokenList["specialSymbols"]:
-                newToken = Token("specialSymbnols", tokenList["specialsymbols"][TokenItem], TokenItem)
-            elif TokenItem[0] == '"' and TokenItem[len(TokenItem) - 1] == '"':
-                newToken = Token('literals', 600, TokenItem)
-            elif isfloat(TokenItem):
-                newToken = Token('literals', 600, TokenItem)
-            else:
-                newToken = Token('UNKNOWN', 1200, TokenItem) #IN case no token is recognized
-
+    for lineTokens in ItemList:
+        for token in lineTokens:
+            categorized_token = categorize_token(token)
+            newToken = Token(categorized_token["Type"], categorized_token["id"], categorized_token["value"])
             finalTokenList.append(newToken)
             print("New Token created: ", newToken.getData())
 
@@ -189,7 +148,18 @@ if __name__ == '__main__':
     loopCounter = 0
     for Token in finalTokenList:
         tokenStr = "Token_" + loopCounter.__str__()
-        megaDict.update({tokenStr: {}})
+    
+        # Create a dictionary for the token with relevant information
+        tokenData = Token.getData()
+        tokenDict = {
+            "Type": tokenData[0],
+            "id": tokenData[1],
+            "value": tokenData[2]
+        }
+    
+        # Add the token dictionary to the megaDict
+        megaDict[tokenStr] = tokenDict
+    
         loopCounter += 1
 
     loopCounter = 0
